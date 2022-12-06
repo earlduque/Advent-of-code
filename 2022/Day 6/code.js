236aaee6b83fbca2179ec7760be3e4f5f7e58e24
Earl Duque <31702109+earlduque@users.noreply.github.com>
2022-12-05 21:47:06 -0800

    day 6

--- 2022/Day 6/code.js
+++ 2022/Day 6/code.js
@@ -2,3 +2,19 @@ const filename = '2022/Day 6/input.txt';
 const fs = require('fs');
 var input = fs.readFileSync(filename, 'utf-8');
 
+//part 1
+function findIt(length){
+    let current;
+    for (var i in input){
+        if (i < length - 1) continue;
+        current = input.substring(i-length,i);
+        if (new Set(current).size == current.length) {
+            console.log(parseInt(i));
+            break;
+        }
+    }
+}
+findIt(4);
+
+//part 2
+findIt(14);
