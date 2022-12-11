9e18ffe4502c73a77d0ca063cea62894a17d6774
Earl Duque <31702109+earlduque@users.noreply.github.com>
2022-12-10 20:16:26 -0800

    so cool

--- 2022/Day 10/part2.js
+++ 2022/Day 10/part2.js
@@ -1,5 +1,5 @@
 const day = 10;
-const use_example = true;
+const use_example = false;
 
 const filename = '2022/Day ' + day + '/' + (use_example ? 'example' : 'input') + '.txt';
 const fs = require('fs');
@@ -7,9 +7,34 @@ var input = fs.readFileSync(filename, 'utf-8');
 
 //part 2
 var input_array = input.split('\n');
-var cycle = 1;
+var instruction_step = 0;
 var register = 1;
+var message = '';
+var to_add = 0;
+var skip_step = false;
 
-for (var i = 1; i <= 240; i++){
-    
+for (let cycle = 0; cycle < 240; cycle++){
+    if (register == cycle%40 || register-1 == cycle%40 || register+1 == cycle%40){
+        message += '#';
+    } else {
+        message += '.';
+    }
+    if (skip_step){
+        register += to_add;
+        instruction_step++;
+    }
+    let instruction = input_array[instruction_step].split(' ');
+    if (!skip_step){
+        if (instruction[0] == 'noop'){
+            instruction_step++;
+        } else {
+            to_add = parseInt(instruction[1]);
+            skip_step = true;
+        }
+    } else {
+        skip_step = false;
+    }
+    if ((cycle + 1) %40 == 0) message +='\n';
 }
+
+console.log(message);
