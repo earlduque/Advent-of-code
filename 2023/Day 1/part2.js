92faf4bde53b40b3a6b2db3d1dcc88f87af86ebf
Earl Duque-HomePC <earl@earlduque.com>
2023-11-29 09:01:19 -0800

    scaffolding for 2023

---
+++ 2023/Day 1/part2.js
@@ -0,0 +1,12 @@
+const day = 1;
+const use_example = true;
+
+const filename = '2023/Day ' + day + '/' + (use_example ? 'example' : 'input') + '.txt';
+const fs = require('fs');
+let og_input = fs.readFileSync(filename, 'utf-8');
+let input_array = og_input.split('\n');
+
+//part 2
+for (let i in input_array){
+    
+}
