dc1d0fe44edcdb2da57117c4a5a65c7cdafed1b9
Earl Duque-HomePC <earl@earlduque.com>
2023-12-03 22:26:31 -0800

    day 5 scaffolding

---
+++ 2023/Day 5/part1.js
@@ -0,0 +1,17 @@
+const useExample = true; // if false, uses input.txt
+const exampleToUse = 1; // 1 or 2
+
+const path = __filename.split("\\"); // Change "\\" to "\/" if you're on mac
+const year = path[path.length-3];
+const day = path[path.length-2].split(' ')[1];
+const fileName = `${year}/Day ${day}/${useExample ? "example" + exampleToUse : "input"}.txt`;
+
+const fs = require('fs');
+let input = fs.readFileSync(fileName, 'utf-8');
+input = input.split('\n');
+
+// Part 1
+
+for (let i in input){
+    
+}
