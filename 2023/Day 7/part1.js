c1de725ab181a6294785625716b3bdc9ca515f66
Earl Duque-HomePC <earl@earlduque.com>
2023-12-06 22:49:14 -0800

    day 7 staying alive stayin alive ah ah ah ah

--- 2023/Day 7/part1.js
+++ 2023/Day 7/part1.js
@@ -1,4 +1,4 @@
-const useExample = true; // if false, uses input.txt
+const useExample = false; // if false, uses input.txt
 const exampleToUse = 1; // 1 or 2
 
 const path = __filename.split("\\"); // Change "\\" to "\/" if you're on mac
@@ -12,6 +12,64 @@ input = input.split('\r\n');
 
 // Part 1
 
-for (let i in input){
+const tiers = [[],[],[],[],[],[],[]];
+
+for (let i of input){
+    let thisHand = i.split(' ');
+    const convertedHand = thisHand[0].replace(/A/g,'E').replace(/K/g,'D').replace(/Q/g,'C').replace(/J/g,'B').replace(/T/g,'A');
+    const sortedHand = thisHand[0].split("").sort().join('');
+    const bid = thisHand[1];
     
+    const repeat = /(.)\1+/g;
+    let thisValue = 0;
+    const kinds = sortedHand.match(repeat);
+
+    let tier = 0
+    if  (kinds == null){
+        tier = 0;
+    } else if (kinds[0].length == 5){
+        tier = 6;
+    } else if (kinds[0].length == 4){
+        tier = 5;
+    } else if (kinds.length == 2 && (kinds[0].length == 3 || kinds[1].length == 3)){
+        tier = 4;
+    } else if (kinds[0].length == 3){
+        tier = 3;
+    } else if (kinds.length == 2){
+        tier = 2;
+    } else if (kinds[0].length == 2){
+        tier = 1;
+    }
+    
+    tiers[tier].push({
+        hand: convertedHand,
+        bid: bid
+    })
 }
+
+// console.log(tiers);
+
+let sorted = [0];
+
+for (let i of tiers){
+    i.sort((a, b) => {
+        if (a.hand > b.hand) {
+            return 1;
+        } else if (a.hand < b.hand) {
+            return -1;
+        }
+        return 0;
+    });
+    for (let j of i){
+        sorted.push(parseInt(j.bid));
+    }
+}
+
+// console.log(sorted);
+
+let total = 0;
+for (let i in sorted){
+    total += i * sorted[i];
+}
+
+console.log(total);
--- 2023/Day 7/part2.js
+++ 2023/Day 7/part2.js
@@ -1,4 +1,4 @@
-const useExample = true; // if false, uses input.txt
+const useExample = false; // if false, uses input.txt
 const exampleToUse = 2; // 1 or 2
 
 const path = __filename.split("\\"); // Change "\\" to "\/" if you're on mac
@@ -12,6 +12,104 @@ input = input.split('\r\n');
 
 // Part 2
 
-for (let i in input){
+const tiers = [[],[],[],[],[],[],[]];
+
+for (let i of input){
+    let thisHand = i.split(' ');
+    const convertedHand = thisHand[0].replace(/A/g,'E').replace(/K/g,'D').replace(/Q/g,'C').replace(/J/g,'0').replace(/T/g,'A');
+    const bid = thisHand[1];
+
+    const repeat = /(.)\1+/g;
+    const preHand = thisHand[0].split("").sort().join('');
+    const preMatch = preHand.match(repeat);
+    let sortedHand;
+    if (preHand.indexOf('J') == -1 || preHand == "JJJJJ") {
+        sortedHand = preHand;
+    } else if (preMatch == null){
+        sortedHand = "AABCD";
+    } else if (preMatch[0].length == 4 || preMatch[0].length == 5){
+        sortedHand = "AAAAA";
+    } else if (preMatch.length == 2 && (preMatch[0].length == 3 || preMatch[1].length == 3)){
+        sortedHand = "AAAAA";
+    } else if (preMatch[0].length == 3){
+        if (preMatch[0] == "JJJ"){
+            sortedHand = "AAAAB";
+        } else {
+            sortedHand = "AAAAB";
+        }
+    } else if (preMatch.length == 2){
+        if (preMatch[0] == "JJ" || preMatch[1] == "JJ"){
+            sortedHand = "BBBBC";
+        } else {
+            sortedHand = "AABBB";
+        }
+    } else if (preMatch[0].length == 2){
+        if (preMatch[0] == "JJ"){
+            sortedHand = "BBBCD";
+        } else {
+            sortedHand = "AAABC";
+        }
+    } else {
+        sortedHand = preHand;
+    }
     
+    const kinds = sortedHand.match(repeat);
+
+    let tier = 0, description;
+    if  (kinds == null){
+        description = 'highcard';
+        tier = 0;
+    } else if (kinds[0].length == 5){
+        description = '5kind';
+        tier = 6;
+    } else if (kinds[0].length == 4){
+        description = '4kind';
+        tier = 5;
+    } else if (kinds.length == 2 && (kinds[0].length == 3 || kinds[1].length == 3)){
+        description = 'fullhouse';
+        tier = 4;
+    } else if (kinds[0].length == 3){
+        description = '3kind';
+        tier = 3;
+    } else if (kinds.length == 2){
+        description = 'twopair';
+        tier = 2;
+    } else if (kinds[0].length == 2){
+        description = '2kind';
+        tier = 1;
+    }
+    
+    tiers[tier].push({
+        hand: convertedHand,
+        bid: bid,
+        what: description,
+        sorted: sortedHand,
+    })
 }
+
+// console.log(tiers);
+
+let sorted = [0];
+
+for (let i of tiers){
+    i.sort((a, b) => {
+        if (a.hand > b.hand) {
+            return 1;
+        } else if (a.hand < b.hand) {
+            return -1;
+        }
+        return 0;
+    });
+    for (let j of i){
+        sorted.push(parseInt(j.bid));
+    }
+}
+
+// console.log(sorted);
+
+let total = 0;
+for (let i in sorted){
+    total += i * sorted[i];
+}
+
+console.log(total);
