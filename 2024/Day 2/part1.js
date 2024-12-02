723e29fe49895e836af49842fe6b6630c4d2df8a
Earl Duque <31702109+earlduque@users.noreply.github.com>
2024-12-01 21:27:23 -0800

    day 2

---
+++ 2024/Day 2/part1.js
@@ -0,0 +1,46 @@
+const useExample = false; // if false, uses input.txt
+const exampleToUse = 1; // 1 or 2
+
+const path = __filename.split("\\"); // Change "\\" to "\/" if you're on mac
+const year = path[path.length-3];
+const day = path[path.length-2].split(' ')[1];
+const fileName = `${year}/Day ${day}/${useExample ? "example" + exampleToUse : "input"}.txt`;
+
+const fs = require('fs');
+let input = fs.readFileSync(fileName, 'utf-8');
+input = input.split('\r\n');
+
+// Part 1
+
+let safe = 0;
+
+for (let i in input){
+    let report = input[i].split(' ');
+
+    report = report.map(Number);
+    console.log(report);
+    if (isGradual(report)) safe++;
+}
+
+console.log(safe);
+
+function isGradual(arr) {
+    let increasing = true;
+    let decreasing = true;
+
+    for (let i = 1; i < arr.length; i++) {
+        const diff = Math.abs(arr[i] - arr[i - 1]);
+
+        if (diff < 1 || diff > 3) {
+            return false;
+        }
+
+        if (arr[i] > arr[i - 1]) {
+            decreasing = false;
+        } else if (arr[i] < arr[i - 1]) {
+            increasing = false;
+        }
+    }
+
+    return increasing || decreasing;
+}
---
+++ 2024/Day 2/part2.js
@@ -0,0 +1,56 @@
+const useExample = false; // if false, uses input.txt
+const exampleToUse = 1; // 1 or 2
+
+const path = __filename.split("\\"); // Change "\\" to "\/" if you're on mac
+const year = path[path.length-3];
+const day = path[path.length-2].split(' ')[1];
+const fileName = `${year}/Day ${day}/${useExample ? "example" + exampleToUse : "input"}.txt`;
+
+const fs = require('fs');
+let input = fs.readFileSync(fileName, 'utf-8');
+input = input.split('\r\n');
+
+// Part 2
+
+let safe = 0;
+
+for (let i in input){
+    let report = input[i].split(' ');
+
+    report = report.map(Number);
+    console.log(report);
+    if (isGradual(report) || isGradualSorta(report)) safe++;
+}
+
+console.log(safe);
+
+function isGradual(arr) {
+    let increasing = true;
+    let decreasing = true;
+
+    for (let i = 1; i < arr.length; i++) {
+        const diff = Math.abs(arr[i] - arr[i - 1]);
+
+        if (diff < 1 || diff > 3) {
+            return false;
+        }
+
+        if (arr[i] > arr[i - 1]) {
+            decreasing = false;
+        } else if (arr[i] < arr[i - 1]) {
+            increasing = false;
+        }
+    }
+
+    return increasing || decreasing;
+}
+
+function isGradualSorta(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        const tempArr = arr.slice(0, i).concat(arr.slice(i + 1));
+        if (isGradual(tempArr)) {
+            return true;
+        }
+    }
+    return false;
+}
